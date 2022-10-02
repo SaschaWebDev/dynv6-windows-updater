08d9752b8ef0a09aee848892eedeb10a894fbd95
Sascha Majewsky <saschawebdev@pm.me>
2022-10-02 18:39:03 +0200

    Refactor

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import Helmet from 'helmet';
 import IP from 'ip';
 import Morgan from 'morgan';
+import cron from 'node-cron';
 import fetch from 'node-fetch';
 
 const server = express();
@@ -25,16 +26,16 @@ server.listen(port, () =>
 );
 
 const updateDynv6 = async (IPv6: string) => {
-	const response = await fetch(
-		`http://dynv6.com/api/update?hostname=${process.env.DYNV6_HOST_NAME}&ipv6=${IPv6}&token=${process.env.DYNV6_TOKEN}`,
-		{
-			method: 'get',
-		}
-	);
-	return response.status === 200 ? true : false;
+	console.log('Updating dynv6.com...');
+	const updateURL = `http://dynv6.com/api/update?hostname=${process.env.DYNV6_HOST_NAME}&ipv6=${IPv6}&token=${process.env.DYNV6_TOKEN}`;
+	const response = await fetch(updateURL, {
+		method: 'get',
+	});
+	return response.status === 200;
 };
 
 const getExternalIPv6 = async () => {
+	console.log('Fetching external IPv6 address...');
 	const response = await fetch(`http://api6.ipify.org`, {
 		method: 'get',
 	});
@@ -44,15 +45,17 @@ const getExternalIPv6 = async () => {
 		: '';
 };
 
-(async () => {
+const tryUpdateDynv6 = async () => {
 	const IPv6Address = await getExternalIPv6();
 	if (IPv6Address) {
 		console.log('External IPv6: ', IPv6Address);
 		const updateResponse = await updateDynv6(IPv6Address);
 		updateResponse
-			? console.log('IPV6 UPDATE FOR DYNV6.COM SUCCESSFULLY')
-			: console.log('IPV6 UPDATE FOR DYNV6.COM FAILED');
+			? console.log('IPv6 UPDATE FOR DYNV6.COM SUCCESSFULL')
+			: console.log('IPv6 UPDATE FOR DYNV6.COM FAILED');
 	} else {
 		console.log('External IPv6 could not be fetched');
 	}
-})();
+};
+
+cron.schedule('*/10 * * * *', () => tryUpdateDynv6()).start();
