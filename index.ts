506698c737909c2cc3c7713b63fbf85cbe0579d8
Sascha Majewsky <saschawebdev@pm.me>
2022-10-02 12:09:55 +0200

    Add typescript to nodejs

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
-require('dotenv').config();
+import 'dotenv/config';
+import express from 'express';
+import Helmet from 'helmet';
+import Morgan from 'morgan';
 
-const server = require('./server');
+const server = express();
+const port = process.env.PORT || 8000;
 
-server.listen(process.env.PORT || 5000, () =>
-	console.log(
-		`dynv6-windows-updater started on port ${process.env.PORT || 5000}`
-	)
+server.use(Helmet());
+server.use(express.json());
+server.use(Morgan('dev'));
+server.disable('etag');
+server.set('json spaces', 4);
+
+server.get('/status', (req, res) => {
+	res.status(200).json({
+		message: `dynv6-windows-updater is running`,
+	});
+});
+
+server.listen(port, () =>
+	console.log(`dynv6-windows-updater started on port ${port}`)
 );
