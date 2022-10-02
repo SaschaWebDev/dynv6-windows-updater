b82ab820e1d8ddfb9f1bde6be52157072b1957dc
Sascha Majewsky <saschawebdev@pm.me>
2022-10-02 16:31:52 +0200

    Add get external ipv6 adress

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import Helmet from 'helmet';
+import http from 'http';
+import IP from 'ip';
 import Morgan from 'morgan';
 
 const server = express();
@@ -12,12 +14,21 @@ server.use(Morgan('dev'));
 server.disable('etag');
 server.set('json spaces', 4);
 
-server.get('/status', (req, res) => {
+server.get('/', (req, res) =>
 	res.status(200).json({
 		message: `dynv6-windows-updater is running`,
-	});
-});
+	})
+);
 
 server.listen(port, () =>
 	console.log(`dynv6-windows-updater started on port ${port}`)
+);
+
+const logIP = (myIp: Object) => console.table(`FETCHED ${myIp}`);
+
+http.get({ host: 'api6.ipify.org', port: 80, path: '/' }, (response) =>
+	response.on(
+		'data',
+		(ip: Object) => IP.isV6Format(String(ip)) && logIP(String(ip))
+	)
 );
